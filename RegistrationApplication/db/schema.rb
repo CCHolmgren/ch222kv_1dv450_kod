55c1e1a3411a0c1d172084447ed2250700f80aaf
Christfoffer Holmgren <ch222kv@student.lnu.se>
2015-02-11 12:28:26 +0100

    Added events and tags as models.

--- RegistrationApplication/db/schema.rb
+++ RegistrationApplication/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206152915) do
+ActiveRecord::Schema.define(version: 20150211112044) do
 
   create_table "api_applications", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,22 @@ ActiveRecord::Schema.define(version: 20150206152915) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "user_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "user_id"
+    t.string   "name"
+    t.string   "short_description"
+    t.text     "description"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
