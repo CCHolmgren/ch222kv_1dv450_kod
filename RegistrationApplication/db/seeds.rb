0f38dcc8fa1c57066dd6e98c364bdb8928ef1a4a
Christfoffer Holmgren <ch222kv@student.lnu.se>
2015-02-11 08:09:49 +0100

    Email is needed, forgot that.

--- RegistrationApplication/db/seeds.rb
+++ RegistrationApplication/db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(username: "admin", password: "password", password_confirmation: "password", is_administrator: true)
+User.create(username: "admin", password: "password", password_confirmation: "password", is_administrator: true, email: "@")
