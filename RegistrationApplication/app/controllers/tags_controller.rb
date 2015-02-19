968e670b32c337f9381f55951185c329904c57da
Christfoffer Holmgren <ch222kv@student.lnu.se>
2015-02-19 15:58:18 +0100

    No CSRF protection on API methods.
    -Tag is validating and provides a better hash of what it contains.
    - Creation of tags is possible.

--- RegistrationApplication/app/controllers/tags_controller.rb
+++ RegistrationApplication/app/controllers/tags_controller.rb
@@ -10,12 +10,19 @@ class TagsController < ApiController
   end
 
   def new
+    @tag = Tag.new
   end
 
   def edit
   end
 
   def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      redirect_to :tags
+    else
+      render :new
+    end
   end
 
   def update
@@ -27,5 +34,8 @@ class TagsController < ApiController
   private
     def set_tag
       @tag = Tag.find(params[:id])
+    end
+    def tag_params
+      params.require(:tag).permit(:name)
     end
 end
--- RegistrationApplication/app/models/tag.rb
+++ RegistrationApplication/app/models/tag.rb
@@ -1,2 +1,9 @@
 class Tag < ActiveRecord::Base
+  validates :name, presence: true, length: {maximum: 50, minimum: 4}
+  def serializable_hash(options={})
+    options = {
+        only: [:name, :created_at, :id]
+    }.update(options)
+    super(options)
+  end
 end
